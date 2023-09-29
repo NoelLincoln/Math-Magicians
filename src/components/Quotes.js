c441db59b39dda444e021a87e37f4bfb2e7e351e
Noel Bryant <noelsobryant@gmail.com>
2023-09-29 22:22:52 +0300

    add loading and error state

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,25 +1,42 @@
 import { useState, useEffect } from 'react';
 
 const Quotes = () => {
-  const [quote, setQuote] = useState([]);
+  const [quote, setQuote] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
       method: 'GET',
       headers: { 'X-Api-Key': 'WZeFsTkEbc8oAfF/xCApuQ==MomspVErh9W30zPP' },
     })
-      .then((response) => response.json())
-      .then((quote) => {
-        setQuote(quote[0].quote);
-        console.log(quote);
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
       })
-      .catch((error) => console.log(error));
+      .then((data) => {
+        setQuote(data[0].quote);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
 
   return (
     <div className="quote">
       <h2>Daily inspiration</h2>
-      {quote && <p>{quote}</p>}
+      {loading && <p>Loading...</p>}
+      {error && (
+        <p>
+          Error:
+          {error}
+        </p>
+      )}
+      {!loading && !error && quote && <p>{quote}</p>}
     </div>
   );
 };
