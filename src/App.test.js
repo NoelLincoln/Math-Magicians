e2731941c5627ee4755241828caea5138a04ecc7
Noel Bryant <noelsobryant@gmail.com>
2023-10-06 00:36:05 +0300

    add tests for operate function

---
+++ src/App.test.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { App } from './App';
+
+// describe('App Component', () => {
+//   it('should have hello world message', () => {
+//     const { getByText } = render(<App />);
+//     expect(getByText('Hello world React!')).toMatchInlineSnapshot(`
+//       <h1>
+//         Hello world React!
+//       </h1>
+//     `);
+//   });
+// });
