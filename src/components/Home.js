375cc683d437e38125c0eeb01a5e66fb88a616b8
Noel Bryant <noelsobryant@gmail.com>
2023-10-05 15:02:27 +0300

    add home page

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import style from '../styles/home.css';
+
+const Home = () => (
+  <section className={style.section}>
+    <h4>Welcome to our page</h4>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum
+      dolores ea quos exercitationem libero et eveniet porro est similique. Eos
+      repudiandae dolore nam veniam itaque voluptas, incidunt necessitatibus
+      maxime.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum
+      dolores ea quos exercitationem libero et eveniet porro est similique. Eos
+      repudiandae dolore nam veniam itaque voluptas, incidunt necessitatibus
+      maxime.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum rerum
+      dolores ea quos exercitationem libero et eveniet porro est similique. Eos
+      repudiandae dolore nam veniam itaque voluptas, incidunt necessitatibus
+      maxime.
+    </p>
+  </section>
+);
+
+export default Home;
