0386ff262cf5d7fed28c46958bf8d2145f5b29a6
Noel Bryant <noelsobryant@gmail.com>
2023-10-05 14:59:02 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quotes from './components/Quotes';
+import Header from './components/Header';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
     <>
-      <Calculator />
-      <Quotes />
+      <Header />
+      <Routes>
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quotes" element={<Quotes />} />
+        <Route path="*" element={<PageNotFound />} />
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </>
   );
 }
