bd691ef611f3e58a9189f7d4c8dfa8afeb60dd7e
Noel Bryant <noelsobryant@gmail.com>
2023-10-06 04:41:57 +0300

    write tests for header component

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,41 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import { App } from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
 
-// describe('App Component', () => {
-//   it('should have hello world message', () => {
-//     const { getByText } = render(<App />);
-//     expect(getByText('Hello world React!')).toMatchInlineSnapshot(`
-//       <h1>
-//         Hello world React!
-//       </h1>
-//     `);
-//   });
-// });
+import App from './App';
+
+test('App component snapshot for the / route', () => {
+  const { asFragment } = render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('App component snapshot for the /calculator route', () => {
+  const { asFragment } = render(
+    <MemoryRouter initialEntries={['/calculator']}>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('App component snapshot for the /quotes route', () => {
+  const { asFragment } = render(
+    <MemoryRouter initialEntries={['/quotes']}>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('App component snapshot for an unknown route', () => {
+  const { asFragment } = render(
+    <MemoryRouter initialEntries={['/unknown']}>
+      <App />
+    </MemoryRouter>,
+  );
+  expect(asFragment()).toMatchSnapshot();
+});
